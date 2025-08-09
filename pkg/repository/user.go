ceec2a7165be7189a3fce53e32368033eb12a051
LeeBee <725lsw@hanmail.net>
2025-08-09 21:35:53 +0900

    feat: user service 로직 구현
    - C,R,U,D 구현

--- pkg/repository/user.go
+++ pkg/repository/user.go
@@ -6,21 +6,84 @@ import (
 	"sync"
 
 	"github.com/google/uuid"
+
+	"github.com/the-end-of-the-human-era-has-arrived/2025-oss-dev-competition-backend/pkg/api"
 	"github.com/the-end-of-the-human-era-has-arrived/2025-oss-dev-competition-backend/pkg/domain"
 )
 
 type MemoryUserRepo struct {
-	mu    sync.RWMutex
-	users map[uuid.UUID]*domain.User
+	mu     sync.RWMutex
+	users  map[uuid.UUID]*domain.User
+	caches map[uuid.UUID]*userCache
+}
+
+type userCache struct {
+	mu               sync.RWMutex
+	deferedOperation []func()
+	users            map[uuid.UUID]*domain.User
 }
 
 func NewMemoryUserRepo() *MemoryUserRepo {
 	return &MemoryUserRepo{
-		users: make(map[uuid.UUID]*domain.User, 1024),
+		users:  make(map[uuid.UUID]*domain.User, 1024),
+		caches: make(map[uuid.UUID]*userCache, 0),
 	}
 }
 
+func (r *MemoryUserRepo) BeginTransaction(ctx context.Context) {
+	requestID, ok := ctx.Value(api.RequestIDKey{}).(uuid.UUID)
+	if !ok {
+		return
+	}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	users := make(map[uuid.UUID]*domain.User)
+	for k, v := range r.users {
+		users[k] = v
+	}
+
+	r.caches[requestID] = &userCache{
+		deferedOperation: make([]func(), 0),
+		users:            users,
+	}
+}
+
+func (r *MemoryUserRepo) Commit(ctx context.Context) {
+	requestID, ok := ctx.Value(api.RequestIDKey{}).(uuid.UUID)
+	if !ok {
+		return
+	}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	cache, ok := r.caches[requestID]
+	if !ok {
+		return
+	}
+	for _, operation := range cache.deferedOperation {
+		operation()
+	}
+}
+
+func (r *MemoryUserRepo) Abort(ctx context.Context) {
+	requestID, ok := ctx.Value(api.RequestIDKey{}).(uuid.UUID)
+	if !ok {
+		return
+	}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	delete(r.caches, requestID)
+}
+
 func (r *MemoryUserRepo) CreateUser(ctx context.Context, user *domain.User) (*domain.User, error) {
+	requestID, ok := ctx.Value(api.RequestIDKey{}).(uuid.UUID)
+	if !ok {
+		return nil, errors.New("not found request id")
+	}
+
 	id, err := uuid.NewRandom()
 	if err != nil {
 		return nil, err
@@ -30,42 +93,99 @@ func (r *MemoryUserRepo) CreateUser(ctx context.Context, user *domain.User) (*do
 
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	r.users[id] = user
+	cache, ok := r.caches[requestID]
+	if !ok {
+		r.users[id] = user
+		return &copied, nil
+	}
 
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	cache.users[id] = user
+	cache.deferedOperation = append(cache.deferedOperation, func() {
+		r.users[id] = user
+	})
 	return &copied, nil
 }
 
 func (r *MemoryUserRepo) FindUserByID(ctx context.Context, id uuid.UUID) (*domain.User, error) {
+	requestID, ok := ctx.Value(api.RequestIDKey{}).(uuid.UUID)
+	if !ok {
+		return nil, errors.New("not found request id")
+	}
+
 	r.mu.RLock()
 	defer r.mu.RUnlock()
+	cache, ok := r.caches[requestID]
+	if !ok {
+		user, ok := r.users[id]
+		if !ok {
+			return nil, errors.New("not found id: " + id.String())
+		}
+		return user, nil
+	}
 
-	user, ok := r.users[id]
+	cache.mu.RLock()
+	defer cache.mu.RUnlock()
+	user, ok := cache.users[id]
 	if !ok {
 		return nil, errors.New("not found id: " + id.String())
 	}
-
 	return user, nil
 }
 
 func (r *MemoryUserRepo) UpdateUser(ctx context.Context, user *domain.User) (*domain.User, error) {
+	requestID, ok := ctx.Value(api.RequestIDKey{}).(uuid.UUID)
+	if !ok {
+		return nil, errors.New("not found request id")
+	}
+
 	copied := *user
 
 	r.mu.Lock()
 	defer r.mu.Unlock()
-	r.users[user.ID] = user
+	cache, ok := r.caches[requestID]
+	if !ok {
+		r.users[user.ID] = user
+		return &copied, nil
+	}
 
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	cache.users[user.ID] = user
+	cache.deferedOperation = append(cache.deferedOperation, func() {
+		r.users[user.ID] = user
+	})
 	return &copied, nil
 }
 
 func (r *MemoryUserRepo) DeleteUserByID(ctx context.Context, id uuid.UUID) (*domain.User, error) {
+	requestID, ok := ctx.Value(api.RequestIDKey{}).(uuid.UUID)
+	if !ok {
+		return nil, errors.New("not found request id")
+	}
+
 	r.mu.Lock()
 	defer r.mu.Unlock()
+	cache, ok := r.caches[requestID]
+	if !ok {
+		deleted, ok := r.users[id]
+		if !ok {
+			return nil, errors.New("not found id: " + id.String())
+		}
+		delete(r.users, id)
+		return deleted, nil
+	}
 
-	deleted, ok := r.users[id]
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+	deleted, ok := cache.users[id]
 	if !ok {
 		return nil, errors.New("not found id: " + id.String())
 	}
-	delete(r.users, id)
-
+	delete(cache.users, id)
+	cache.deferedOperation = append(cache.deferedOperation, func() {
+		delete(r.users, id)
+	})
 	return deleted, nil
 }
---
+++ pkg/service/user.go
@@ -0,0 +1,81 @@
+package service
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"github.com/the-end-of-the-human-era-has-arrived/2025-oss-dev-competition-backend/pkg/domain"
+	"github.com/the-end-of-the-human-era-has-arrived/2025-oss-dev-competition-backend/pkg/repository"
+)
+
+type UserService struct {
+	repo *repository.MemoryUserRepo
+}
+
+func NewUserService(repo *repository.MemoryUserRepo) *UserService {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+func (s *UserService) CreateUser(ctx context.Context, user *domain.User) error {
+	if _, err := s.repo.CreateUser(ctx, user); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) GetUser(ctx context.Context, id uuid.UUID) (*domain.User, error) {
+	return s.repo.FindUserByID(ctx, id)
+}
+
+func (s *UserService) UpdateNickname(ctx context.Context, id uuid.UUID, nickname string) error {
+	s.repo.BeginTransaction(ctx)
+	defer s.repo.Commit(ctx)
+
+	user, err := s.repo.FindUserByID(ctx, id)
+	if err != nil {
+		s.repo.Abort(ctx)
+		return err
+	}
+
+	user.Nickname = nickname
+
+	if _, err := s.repo.UpdateUser(ctx, user); err != nil {
+		s.repo.Abort(ctx)
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) UpdateTokens(
+	ctx context.Context,
+	id uuid.UUID,
+	accessToken, refreshToken string,
+) error {
+	s.repo.BeginTransaction(ctx)
+	defer s.repo.Commit(ctx)
+
+	user, err := s.repo.FindUserByID(ctx, id)
+	if err != nil {
+		s.repo.Abort(ctx)
+		return err
+	}
+
+	user.AccessToken = accessToken
+	user.RefreshToken = refreshToken
+
+	if _, err := s.repo.UpdateUser(ctx, user); err != nil {
+		s.repo.Abort(ctx)
+		return err
+	}
+	return nil
+}
+
+func (s *UserService) DeleteUser(ctx context.Context, id uuid.UUID) error {
+	if _, err := s.repo.DeleteUserByID(ctx, id); err != nil {
+		return err
+	}
+	return nil
+}
